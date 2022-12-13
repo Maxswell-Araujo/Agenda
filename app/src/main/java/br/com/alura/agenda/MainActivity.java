579eced55f3130a3a43d64e0e4f0f61edb90d9af
Maxswell-Araujo <maxswell.developer@gmail.com>
2022-12-12 23:20:49 -0300

    listView

--- app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -1,15 +1,30 @@
 package br.com.alura.agenda;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends Activity {
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Maxswell", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Alex", "Fran", "José"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+
+
     }
 }
