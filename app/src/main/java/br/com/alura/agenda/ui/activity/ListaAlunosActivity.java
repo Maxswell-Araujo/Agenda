5c9d940dde1ede5fc70a942de8ea25df74bbd5b2
Maxswell Emanoell Araújo de Melo <mamelo@mpac.local>
2022-12-14 15:44:35 -0300

    UI Activity

--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -5,33 +5,32 @@ Nesta aula, aprendemos a:
     implementar layouts que apresentam listas;
     utilizar AdapterViews em específico o ListView.
  */
-package br.com.alura.agenda;
+package br.com.alura.agenda.ui.activity;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+import br.com.alura.agenda.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
+        setTitle("Lista de Alunos");
         List<String> alunos = new ArrayList<>(
                 Arrays.asList("Alex", "Fran", "José", "Maxswell"));
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
